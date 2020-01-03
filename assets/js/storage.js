d2c583414753127be12b182694bdbb307b2dcb16
Andrew Leedham <AndrewLeedham@Outlook.com>
2020-01-03 16:42:11 +0000

    refactor: format js

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,14 +1,12 @@
 'use strict';
 
 module.exports = {
+  get(name, fallback) {
+    const result = localStorage.getItem(name);
+    return result ? JSON.parse(result) : fallback;
+  },
 
-    get(name, fallback) {
-        const result = localStorage.getItem(name);
-        return result ? JSON.parse(result) : fallback;
-    },
-
-    set(name, value) {
-        localStorage.setItem(name, JSON.stringify(value));
-    }
-
+  set(name, value) {
+    localStorage.setItem(name, JSON.stringify(value));
+  },
 };
