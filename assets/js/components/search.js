79599f1e020e64e77f7aed2ad61642139cc06440
Andrew Leedham <AndrewLeedham@Outlook.com>
2020-01-07 23:46:05 +0000

    docs: comments and cleanup

--- assets/js/components/search.js
+++ assets/js/components/search.js
@@ -8,22 +8,26 @@ const whitespaceRegex = /^\s*$/;
 
 class Search {
   constructor(el, navTrees) {
-    this._navTrees = navTrees;
     this._el = $(el);
+
+    // Get search text from local storage and set search field value.
     const initialValue = storage.get('search.value', '');
     this._el.attr('value', initialValue);
 
+    // Assemble all of the trees.
     this.trees = navTrees.map((navTree) => ({
       navTree,
       searchTree: this.createSearchTree(navTree._el),
     }));
 
+    // If the initial search text is not blank show/hide matching items.
     if (initialValue !== '') {
       for (const tree of this.trees) {
         this.updateTree(tree, initialValue);
       }
     }
 
+    // Add an event listener for user input to show/hide matching items.
     this._el.on(
       'input',
       utils.debounce((e) => {
@@ -36,10 +40,19 @@ class Search {
     );
   }
 
+  /**
+   * Creates an object tree structure for the given nav tree.
+   *
+   * @param {JQuery} element - Nav tree or tree item jQuery reference.
+   * @param {Object} parent - Parent tree for nested structure (used internally for recursion).
+   * @returns A fully formed search tree.
+   */
   createSearchTree(element, parent = undefined) {
     const tree = {
       parent,
+      // Store the current jQuery reference.
       element: parent && element,
+      // Add the current label for search checking later down the line.
       text:
         parent &&
         element
@@ -49,6 +62,7 @@ class Search {
           .toLowerCase(),
     };
 
+    // Create sub trees for all the child tree items.
     tree.children = $.map(element.find('> ul > li.Tree-item'), (child) =>
       this.createSearchTree($(child), tree)
     );
@@ -56,19 +70,30 @@ class Search {
     return tree;
   }
 
+  /**
+   * Shows/hides members of the given tree if they match the given phrase.
+   *
+   * @param {Object} tree - The tree structure to update.
+   * @param {*} rawPhrase - The raw search field phrase.
+   */
   updateTree(tree, rawPhrase) {
     const { navTree, searchTree } = tree;
     const phrase = rawPhrase.trim().toLowerCase();
+    // Check if the phrase is just whitespace.
     const invalidPhrase = whitespaceRegex.test(phrase);
+    // Reset the nav tree state if we are not performing a search.
     if (invalidPhrase) {
       navTree._applyState();
     }
     const queue = [{ item: searchTree, showState: false }];
+    // Loop while there are still items in the queue.
     while (queue.length > 0) {
       let { item: current, showState } = queue.shift();
       let found;
       if (!invalidPhrase) {
+        // Check if the current item text matches the phrase.
         found = current.text && current.text.includes(phrase);
+        // If we have a match traverse all the way up showing each element.
         if (found) {
           navTree.expandAll(true);
           let parent = current.parent;
@@ -79,12 +104,15 @@ class Search {
         }
       }
       const show = invalidPhrase || found || showState;
+      // Show/hide the current element/
       if (show) {
+        // A show state of true will be passed to all children allowing collections to be matched.
         showState = true;
         current.element && current.element.show();
       } else if (!showState) {
         current.element && current.element.hide();
       }
+      // Add all the current children to the queue.
       for (const child of current.children) {
         queue.push({ item: child, showState: show });
       }
