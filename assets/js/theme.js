256a356584f82b4322e8bfd7539ed19018a57e59
Tim <tim.ottewell@xerox.com>
2020-05-01 21:55:26 +0100

    feat: use ajax still needs tidying

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -22,6 +22,8 @@ const navTrees = $.map($('[data-behaviour="tree"]'), (t) => new Tree(t));
 const search = new Search($('[data-behaviour="search"]'), navTrees);
 let pens = [];
 
+let selectedComponent;
+
 loadPen();
 
 if (frctl.env === 'server') {
@@ -44,7 +46,33 @@ if (frctl.env === 'server') {
     .on('pjax:end', function () {
       events.trigger('main-content-loaded');
       frame.endLoad();
+    })
+    .on('click', '.Pen-variant-link', function (e) {
+      selectedComponent = e.currentTarget;
+      const variantUrl = selectedComponent.href;
+      if (variantUrl && !$(this).hasClass('active')) {
+        const component = $(this);
+        $('.Pen-panel.Pen-preview').addClass('loading');
+        $('.Pen-panel.Pen-preview').load(
+          variantUrl + ' .Pen-panel.Pen-preview .Preview-wrapper',
+          function () {
+            events.trigger('main-content-loaded');
+            switchComponent();
+            $('.Pen-panel.Pen-preview').removeClass('loading');
+          }
+        );
+      }
+      e.preventDefault();
     });
+
+  $(window).on('popstate', function (e) {
+    $('.Pen-variant-link.active').removeClass('active');
+    $('.Pen-variant-link[href="' + e.state.selectedHref + '"]').addClass(
+      'active'
+    );
+    $('.Pen-panel.Pen-preview').html(e.state.componentHtml);
+    events.trigger('main-content-loaded');
+  });
 }
 
 events.on('main-content-loaded', loadPen);
@@ -53,4 +81,27 @@ function loadPen() {
   setTimeout(function () {
     pens = $.map($('[data-behaviour="pen"]'), (p) => new Pen(p));
   }, 1);
+}
+
+function trackHistory(currentComponent, url) {
+  const selectedHref = $('.Pen-variant-link.active').attr('href');
+  window.history.pushState(
+    { componentHtml: currentComponent, selectedHref: selectedHref },
+    '',
+    url
+  );
+}
+
+function switchComponent() {
+  console.log(selectedComponent);
+  let url;
+  if (selectedComponent) {
+    $('.Pen-variant-link.active').removeClass('active');
+    $(selectedComponent).addClass('active');
+    url = selectedComponent.href;
+  } else {
+    url = window.location.href;
+  }
+  const currentComponent = $('.Pen-panel.Pen-preview').html();
+  trackHistory(currentComponent, url);
 }
